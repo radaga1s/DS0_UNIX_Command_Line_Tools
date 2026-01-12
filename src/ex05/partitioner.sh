687c06d26da56c3aed502e5fec6c6a869cd3909e
radaga1s <54602590+radaga1s@users.noreply.github.com>
2026-01-12 16:30:49 +0300

    minor changes

--- src/ex05/partitioner.sh
+++ src/ex05/partitioner.sh
@@ -3,7 +3,6 @@
 SRC_FILE="../ex03/hh_positions.csv"
 HEADER=$(head -n 1 "$SRC_FILE")
 PARTS_FOLDER="parts"
-
 mkdir -p "$PARTS_FOLDER"
 tail -n +2 "$SRC_FILE" | awk -F, -v header="$HEADER" -v parts_folder="$PARTS_FOLDER" '{
     split($2, date_time, "T");
